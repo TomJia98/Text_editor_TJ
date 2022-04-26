5fda4f8975e7c0d0df11b76f9c8d2f40608d010c
Tom Jia <tomchristian1998@gmail.com>
2022-04-26 18:22:00 +0800

    got the install working, just need to fix the button

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: "development",
@@ -36,6 +33,7 @@ module.exports = () => {
         background_color: "#ffffff",
         crossorigin: "use-credentials",
         start_url: "/",
+        id: "/",
         publicPath: "/",
         icons: [
           {
